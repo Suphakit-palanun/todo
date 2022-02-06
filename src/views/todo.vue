67528e359c4857d9d210ec54f63738fca5774cad
Supakit-palanun <you@example.com>
2022-02-06 21:13:02 +0700

    first commit

---
+++ src/views/todo.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hellotodo></Hellotodo>
+</template>
+
+<script>
+import Hellotodo from "../components/Hellotodo.vue"
+export default {
+components:{Hellotodo},
+}
+</script>
+
+<style>
+
+</style>
